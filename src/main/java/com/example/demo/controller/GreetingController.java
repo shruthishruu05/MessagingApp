5d270d177ff6b36267b694f02e52bd36c1dede27
shruthishruu05 <shruthi.m@perfios.com>
2021-10-31 18:42:27 +0530

    ADDED : Ability to return JSON for different HTTP Methods

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -4,8 +4,12 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +24,20 @@ public class GreetingController {
 	    public Greeting greeting(@RequestParam(value = "name",defaultValue = "World")String name){
 	        return new Greeting(counter.incrementAndGet(),String.format(template,name));
 	 }
+	 @RequestMapping(value= {"/query"}, method = RequestMethod.GET)
+		public String sayHello(@RequestParam(value="name") String name) {
+			return "Hello "+name+" from BridgeLabz!";
+		}
+		
 	 
 	 @PostMapping("/postGreeting")
 	    public Greeting sayHello(@RequestBody Greeting greeting){
 	        return new Greeting(counter.incrementAndGet(),String.format(template,greeting.getContent()));
 	 }
+	 
+	 @PutMapping("/putGreeting/{counter}")
+	    public Greeting sayHello(@PathVariable int counter,@RequestParam(value = "content")String contentName){
+	        return new Greeting(counter,String.format(template,contentName));
+	    }
+	 
 }
