261b02c15d4601528de09f2f80d35cb67750a41b
shruthishruu05 <shruthi.m@perfios.com>
2021-11-02 00:05:10 +0530

    Ability to add greeting service to get greetng by id

--- src/main/java/com/example/demo/service/GreetingService.java
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -1,10 +1,15 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.NewGreeting;
 import com.example.demo.model.Greeting;
 import com.example.demo.model.User;
 import com.example.demo.repository.GreetingRepository;
@@ -15,6 +20,7 @@ public class GreetingService implements IGreetingService {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
+	
 	@Autowired
 	private GreetingRepository greetingRepository;
 	 
@@ -22,12 +28,13 @@ public class GreetingService implements IGreetingService {
 	public Greeting addGreeting(User user) 
 	{
 		String message = String.format(template, (user.toString().isEmpty())? "Hello World": user.toString());
+		Greeting greeting = new Greeting(counter.incrementAndGet(),message);
 		return greetingRepository.save(new Greeting(counter.incrementAndGet(),message));
 	}
 
 	@Override
-	public Greeting getGreetingById(long id) {
-		return null;
+	public Optional<Greeting> getGreetingById(long id) {
+		return greetingRepository.findById(id);
 	}
 
 }
