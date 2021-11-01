e4577beb9ab9d62206ba17b4373a7bba76abd4fe
shruthishruu05 <shruthi.m@perfios.com>
2021-11-02 00:07:45 +0530

    Ability to add greeting service interface

--- src/main/java/com/example/demo/service/IGreetingService.java
+++ src/main/java/com/example/demo/service/IGreetingService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
 import com.example.demo.model.Greeting;
 import com.example.demo.model.User;
 
 public interface IGreetingService {
 	Greeting addGreeting(User user);
-	Greeting getGreetingById(long id);
+	Optional<Greeting> getGreetingById(long id);
 }
